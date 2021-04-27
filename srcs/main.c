88b46ec7c03cb648f70983912f2866806595643a
Herminia Woodwright <hwoodwri@ev-b6.kzn.21-school.ru>
2021-04-27 22:07:55 +0300

    NACHALO

---
+++ srcs/main.c
@@ -0,0 +1,12 @@
+#include "../includes/minishell.h"
+
+int main()
+{
+    char buf[1024];
+    while (1)
+    {
+        write(1, "\e[32mminishell> \e[0m", 20);
+        read(0, buf, 1024);
+        //то, что в buf - идет в парсер
+    }
+}
