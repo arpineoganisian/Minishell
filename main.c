b498eb5d16ba1f49c391b35051b22a9ec56d0dee
Herminia Woodwright <hwoodwri@ev-b6.kzn.21-school.ru>
2021-04-27 21:20:31 +0300

    NACHALO

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main()
     char buf[1024];
     while (1)
     {
-        write(1, "minishell> ", 11);
+        write(1, "\e[32mminishell> \e[0m", 20);
         read(0, buf, 1024);
         //то, что в buf идет в парсер
     }
