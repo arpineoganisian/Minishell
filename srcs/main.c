f854d01e106ff22dfd393ad9e8ca7ea90a24c1a0
Herminia Woodwright <hwoodwri@ev-b6.kzn.21-school.ru>
2021-04-28 15:53:45 +0300

    modified main.c minishell.h

--- srcs/main.c
+++ srcs/main.c
@@ -1,12 +1,29 @@
 #include "../includes/minishell.h"
 
-int main()
+int main(int argc, char **argv, char **envp)
 {
-    char buf[1024];
-    while (1)
+    (void)argc;
+    (void)argv;
+    (void)envp;
+
+    // envp = нуль-терминальный массив указателей на строки переменных окружения.
+    // Каждая строка в формате ИМЯ=ЗНАЧЕНИЕ
+
+    int i;
+
+    i = 0;
+    while(envp[i])
     {
-        write(1, "\e[32mminishell> \e[0m", 20);
-        read(0, buf, 1024);
-        //то, что в buf - идет в парсер
+        printf("%s\n", envp[i]);
+        i++;
     }
+
+
+    // char buf[1024];
+    // while (1)
+    // {
+    //     write(1, "\e[32mminishell> \e[0m", 20);
+    //     read(0, buf, 1024);
+    //     //то, что в buf - идет в парсер
+    // }
 }
