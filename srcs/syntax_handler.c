3adcd45989f4fd70c4aa9bf338e6612211dc707c
Misha Bobrov <mshbbrv@gmail.com>
2021-07-02 14:05:23 +0300

    raw

--- srcs/syntax_handler.c
+++ srcs/syntax_handler.c
@@ -3,14 +3,23 @@
 int	check_syntax(const char *str)
 {
 	int	i;
+	int	tmp;
 
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == '|' && (str[i - 1] < 0 || str[i + 1] == 0 || str[i + 1]
-		== '|'))
+		if (str[i] == '|')
 		{
-			error_handler("syntax error near unexpected token `|'");
+			tmp = i;
+			while (str[i] == ' ')
+				i--;
+			if (str[i] < 0)
+				error_handler("syntax error near unexpected token `|'");
+			i = tmp;
+			while (str[i] == ' ')
+				i++;
+			if (str[i] == '|' || str[i] == '\0')
+				error_handler("syntax error near unexpected token `|'");
 			return (1);
 		}
 		if (str[i])
