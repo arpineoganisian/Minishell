cf80a37fec6eb88e2a11c6b642fb4afc38fa72ca
Misha Bobrov <mshbbrv@gmail.com>
2021-07-02 18:52:46 +0300

    raw

--- srcs/execute_cmd.c
+++ srcs/execute_cmd.c
@@ -5,6 +5,7 @@ void	execute_cmd(char **cmd_line, t_data *data)
 	int	i;
 	(void)data;
 
+	printf("fd = %d ", data->fd);
 	if (!ft_strncmp(cmd_line[0], "echo", ft_strlen("echo")))
 	{
 		echo(cmd_line);
