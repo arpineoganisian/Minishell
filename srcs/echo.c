cc643a530cb6281ac441b8beffc5ac974a22d8c5
Arpine <hwoodwri@gmail.com>
2021-07-06 16:15:16 +0300

    сделала эхо

--- srcs/echo.c
+++ srcs/echo.c
@@ -4,19 +4,25 @@ int		check_flag(char **cmd_line, int *i)
 {
 	int j;
 
-	j = 1;
-	if(ft_strncmp(cmd_line[*i], "-n", 2 != 0))
+	j = 2;
+	if(ft_strncmp(cmd_line[*i], "-n", 2 != 0) || ft_strlen(cmd_line[*i]) < 2)
 		return 0;
-	while(cmd_line[(*i)++])
+	while(cmd_line[(*i)])
 	{
 		if (ft_strncmp(cmd_line[*i], "-n", 2 != 0))
 			return 1;
 		else
-			while(cmd_line[*i][j++])
+		{
+			while (cmd_line[*i][j])
+			{
 				if (cmd_line[*i][j] != 'n')
 					return 1;
+				j++;
+			}
+		}
+		(*i)++;
 	}
-	return 0;
+	return 1;
 }
 
 void    echo(char **cmd_line)
