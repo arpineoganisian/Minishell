a7e524f416178d959f7c0304bce87d4a11f4a0ad
Arpine <hwoodwri@gmail.com>
2021-07-06 16:15:37 +0300

    сделала эхо

--- srcs/execute_cmd.c
+++ srcs/execute_cmd.c
@@ -5,12 +5,12 @@ void	execute_cmd(char **cmd_line, t_data *data)
 	int	i;
 	(void)data;
 
-//	ft_putstr_fd("fd output = ", 1);
-//	ft_putnbr_fd(data->fd_out, 1);
-//	ft_putstr_fd(" ", 1);
-//	ft_putstr_fd("fd input = ", 1);
-//	ft_putnbr_fd(data->fd_in, 1);
-//	ft_putstr_fd(" ", 1);
+	ft_putstr_fd("fd output = ", 1);
+	ft_putnbr_fd(data->fd_out, 1);
+	ft_putstr_fd(" ", 1);
+	ft_putstr_fd("fd input = ", 1);
+	ft_putnbr_fd(data->fd_in, 1);
+	ft_putstr_fd(" ", 1);
 	if (data->fd_out != 1)
 		close(data->fd_out);
 	data->fd_out = 1;
@@ -20,14 +20,14 @@ void	execute_cmd(char **cmd_line, t_data *data)
 	if (!ft_strncmp(cmd_line[0], "echo", ft_strlen("echo")))
 	{
 		echo(cmd_line);
-//		printf("cmd = %s ", cmd_line[0]);
-//		i = 1;
-//		while (cmd_line[i])
-//		{
-//			printf("| %d arg = |%s| ", i, cmd_line[i]);
-//			i++;
-//		}
-//		printf("\n");
+		printf("cmd = %s ", cmd_line[0]);
+		i = 1;
+		while (cmd_line[i])
+		{
+			printf("| %d arg = |%s| ", i, cmd_line[i]);
+			i++;
+		}
+		printf("\n");
 	}
 	else if (!ft_strncmp(cmd_line[0], "cd", ft_strlen("cd")))
 	{
