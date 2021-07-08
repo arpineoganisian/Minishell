f7bd950407e864d36ca2cf5d41cf643bd435d727
Arpine <hwoodwri@gmail.com>
2021-07-08 16:10:07 +0300

    cd is done

--- srcs/cd.c
+++ srcs/cd.c
@@ -1,42 +1,43 @@
 #include "minishell.h"
 
-int cd_home()
+void	cd_err_handler(char *str1, char *str2, char *cmd_line)
 {
-	int chdir_ret_val;
-	char *home;
+	char	*error;
+	char	*tmp;
+
+	tmp = ft_strjoin(str1, cmd_line);
+	error = ft_strjoin(tmp, str2);
+	error_handler(ft_strjoin(error, strerror(errno)), 1);
+	free(tmp);
+	free(error);
+}
+
+int	cd_home(void)
+{
+	char	*home;
 
 	home = getenv("HOME");
 	if (!home)
 	{
-		ft_putendl_fd("cd: HOME not set", 2);
-		return 1;
+		ft_putendl_fd("minishell: cd: HOME not set", 2);
+		return (1);
 	}
-	chdir_ret_val = chdir(home);
-	if (chdir_ret_val == -1)
+	if (chdir(home) == -1)
 	{
 		ft_putendl_fd(strerror(errno), 2);
-		return 1;
+		return (1);
 	}
-	return 0;
+	return (0);
 }
 
-int cd(char **cmd_line, t_data *data)
+int	cd(char **cmd_line)
 {
-	int chdir_ret_val;
-	char	*error;
-	char	*tmp;
-
 	if (strings_counter(cmd_line) == 1)
-		return cd_home();
-	chdir_ret_val = chdir(cmd_line[1]);
-	if (chdir_ret_val == -1)
+		return (cd_home());
+	if (chdir(cmd_line[1]) == -1)
 	{
-		tmp = ft_strjoin("cd: ", cmd_line[1]);
-		error = ft_strjoin(tmp, ": ");
-		error_handler(ft_strjoin(error, strerror(errno)), data, 1);
-		free(tmp);
-		free(error);
-		return 1;
+		cd_err_handler("cd: ", ": ", cmd_line[1]);
+		return (1);
 	}
-	return 0;
+	return (0);
 }
