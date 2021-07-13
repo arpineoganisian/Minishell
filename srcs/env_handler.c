b893d3e757510b7df4ad46384205f87e03452811
Misha Bobrov <mshbbrv@gmail.com>
2021-07-13 15:42:22 +0300

    raw

--- srcs/env_handler.c
+++ srcs/env_handler.c
@@ -1,9 +1,36 @@
 #include "minishell.h"
 
-char	*key_not_found(char *str, int start, int end)
+int  env_key_finder(char **envp, char *env_key)
 {
-	char	*before_variable;
-	char	*new_str;
+	int  i;
+	int  k;
+	char *current_key;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strnstr(envp[i], env_key, ft_strlen(env_key)))
+		{
+			k = 0;
+			while (envp[i][k] != '=')
+				k++;
+			current_key = ft_substr(envp[i], 0, k);
+			if (!ft_strncmp(env_key, current_key, ft_strlen(current_key)))
+			{
+				free(env_key);
+				free(current_key);
+				return (i);
+			}
+		}
+		i++;
+	}
+	return (-1);
+}
+
+char *key_not_found(char *str, int start, int end)
+{
+	char *before_variable;
+	char *new_str;
 
 	before_variable = ft_substr(str, 0, start);
 	new_str = ft_strjoin(before_variable, str + end);
@@ -12,45 +39,46 @@ char	*key_not_found(char *str, int start, int end)
 	return (new_str);
 }
 
-char 	*key_found(char *value, char *str, int start, int end)
+char  *key_found(char *env_str, char *str, int start, int end)
 {
-	char	*new_str;
-	char	*merge_str;
+	char *env_value;
+	char  *new_str;
+	char  *merge_str;
+	int  i;
 
+	i = 0;
+	while (env_str[i] != '=')
+		i++;
+	env_value = ft_substr(env_str, i + 1, ft_strlen(env_str) - i);
 	new_str = ft_substr(str, 0, start);
-	merge_str = ft_strjoin(new_str, value);
+	merge_str = ft_strjoin(new_str, env_value);
 	if (new_str)
 		free(new_str);
+	if (env_value)
+		free(env_value);
 	new_str = ft_strjoin(merge_str, str + end);
 	if (merge_str)
 		free(merge_str);
 	return (new_str);
 }
 
-char	*env_handler(char *str, int *i)
+char *env_handler(char *str, int *i, char **envp)
 {
-	char	*env_key;
-	char	*new_str;
-	int		start;
-	char	*value;
+	char *env_key;
+	char *new_str;
+	int  start;
+	int  line_number;
 
 	start = *i;
 	(*i)++;
 	while (str[*i] == '_' || ft_isalnum(str[*i]) || str[*i] == '?')
 		(*i)++;
 	env_key = ft_substr(str, start + 1, *i - start - 1);
-	if (!ft_strncmp(env_key, "?", ft_strlen(env_key)))
-		value = ft_itoa(exit_status);
-	else
-		value = getenv(env_key);
-	//TODO "echo $PWD" - обрабатывается здесь?
-	// если здесь, тогда вместо get_env(ключ)
-	// нужен
-	// data->envp[find_env_var(ключ, data)]
-	if (!value)
+	line_number = env_key_finder(envp, env_key);
+	if (line_number == -1)
 		new_str = key_not_found(str, start, *i);
 	else
-		new_str = key_found(value, str, start, *i);
+		new_str = key_found(envp[line_number], str, start, *i);
 	*i = start;
 	return (new_str);
 }
