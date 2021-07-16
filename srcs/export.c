e65d59ac3fd9c65ff88bdca296f5669f2dd69973
Arpine <hwoodwri@gmail.com>
2021-07-16 18:14:29 +0300

    export with no arg

--- srcs/export.c
+++ srcs/export.c
@@ -1,5 +1,53 @@
 #include "minishell.h"
 
+char	**sort_envp(char **envp)
+{
+	int		i;
+	int		j;
+	char	**sorted;
+	int		n;
+	char	*tmp;
+
+	i = 0;
+	n = strings_counter(envp);
+	sorted = copy_envp(envp);
+	while (i < n - 1)
+	{
+		j = i + 1;
+		while (j < n)
+		{
+			if (ft_strncmp(sorted[i], sorted[j], ft_strlen(sorted[i])) > 0)
+			{
+				tmp = ft_strdup(sorted[i]);
+				free(sorted[i]);
+				sorted[i] = ft_strdup(sorted[j]);
+				free(sorted[j]);
+				sorted[j] = ft_strdup(tmp);
+				free(tmp);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (sorted);
+}
+
+void	print_export(char **envp)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+
+	while(envp[i])
+	{
+		while();
+		i++;
+	}
+	free(envp);
+}
+
 int	check_and_change_env_vars(char *cmd_line, t_data *data)
 {
 	char	*key;
@@ -39,9 +87,11 @@ void	add_env_var(t_data *data, char *cmd_line)
 
 int	export(char **cmd_line, t_data *data)
 {
-	//todo вывод просто команды export
 	if (strings_counter(cmd_line) == 1)
+	{
+		print_export(sort_envp(data->envp));
 		return (0);
+	}
 	if (cmd_line[1][0] == '=')
 	{
 		error_handler(ft_strjoin(ft_strjoin("export: `", cmd_line[1]), "': not a valid identifier"), 1);
